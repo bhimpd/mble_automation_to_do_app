515899232e53e693f98da86f5eb95a33aa58cc6b
bhimpd <dreamypd73@gmail.com>
2026-03-28 10:23:59 +0545

    added regression and parallel testing

--- devices.ts
+++ devices.ts
@@ -1,9 +1,9 @@
 export const devices = [
-    // {
-    //     name: 'Redmi',
-    //     udid: 'u8g6wcmzuk9x65rs',
-    //     systemPort: 8201
-    // }
+    {
+        name: 'Redmi',
+        udid: 'u8g6wcmzuk9x65rs',
+        systemPort: 8201
+    },
     {
         name: 'Honor',
         udid: 'BBPBB19416200375',
